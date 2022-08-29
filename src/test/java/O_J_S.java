7fd5576f8d3c725e37684b97d4123674fdd8f318
hp <hp@LAPTOP-L8EE4T9K>
2022-08-29 18:30:10 +0530

    Ye hai CHutiyo ke liye

---
+++ src/test/java/O_J_S.java
@@ -0,0 +1,9 @@
+
+public class O_J_S {
+
+	public static void main(String[] args) {
+
+		System.out.println("Teri MKC");
+	}
+
+}
---
+++ src/test/java/S_Parhe.java
@@ -0,0 +1,9 @@
+
+public class S_Parhe {
+
+	public static void main(String[] args) {
+
+		System.out.println("This is the Bullshit gye");
+	}
+
+}
